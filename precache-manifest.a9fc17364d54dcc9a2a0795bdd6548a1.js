72ed2c6f7827de82eb4e296163d7cbd5170a6ed0
Kai Tait <kaiftait@gmail.com>
2019-11-14 19:03:15 +1000

    Updates

---
+++ precache-manifest.a9fc17364d54dcc9a2a0795bdd6548a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9d65e61e09aa255a0a3116a0b34d09",
+    "url": "/disease-classifier/index.html"
+  },
+  {
+    "revision": "e71515167cd82e3ecbf8",
+    "url": "/disease-classifier/static/css/main.f52e7724.chunk.css"
+  },
+  {
+    "revision": "b481bda22e7a6aab9ac8",
+    "url": "/disease-classifier/static/js/2.4b8e3b33.chunk.js"
+  },
+  {
+    "revision": "e71515167cd82e3ecbf8",
+    "url": "/disease-classifier/static/js/main.53fe6563.chunk.js"
+  },
+  {
+    "revision": "2c8216b967c4bbc94ad5",
+    "url": "/disease-classifier/static/js/runtime-main.82c45eb7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/disease-classifier/static/media/logo.25bf045c.svg"
+  }
+]);
